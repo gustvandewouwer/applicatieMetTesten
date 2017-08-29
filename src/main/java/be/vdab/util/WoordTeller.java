4f86e59886d6cc2a10c3d5a8bf6fa9a8999133d4
Gust Van de Wouwer <gust.vandewouwer@gmail.com>
2017-08-29 09:02:21 +0200

    No comment

--- src/main/java/be/vdab/util/WoordTeller.java
+++ src/main/java/be/vdab/util/WoordTeller.java
@@ -4,10 +4,6 @@ import java.util.StringTokenizer;
 
 
 public class WoordTeller {
-	/**
-	 * @param zin
-	 * @return
-	 */
 	public int telWoorden(String zin) {
 		return new StringTokenizer(zin, " ,").countTokens();
 	}
